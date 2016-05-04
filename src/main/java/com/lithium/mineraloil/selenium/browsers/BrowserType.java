997f980ccadab7497cc40a37f65b472a4b3e00f2
Donald Johnstone <donald.johnstone@lithium.com>
2016-05-04 16:08:52 -0500

    remove not needed semi-colon

--- src/main/java/com/lithium/mineraloil/selenium/browsers/BrowserType.java
+++ src/main/java/com/lithium/mineraloil/selenium/browsers/BrowserType.java
@@ -1,5 +1,5 @@
 package com.lithium.mineraloil.selenium.browsers;
 
 public enum BrowserType {
-    FIREFOX, CHROME, REMOTE_CHROME, REMOTE_FIREFOX;
+    FIREFOX, CHROME, REMOTE_CHROME, REMOTE_FIREFOX
 }
