a25d1a8a870697206a90e352276efdda6b585b7d
hugh.mcgowan <hugh.mcgowan@lithium.com>
2016-08-25 10:08:08 -0500

    add release branch

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -o errexit
+RELEASE_BRANCH="release"
 DEVELOP_BRANCH="master"
-TAG_PREFIX="mineraloil-selenium"
+TAG_PREFIX="analytics"
 WORK_PREFIX="rel"
 if [ "$#" != "2" ]; then
     echo "Must provide 2 arguments:"
@@ -12,12 +13,17 @@ fi
 # Make sure master and release branches are up to date
 git checkout $DEVELOP_BRANCH
 git fetch origin
+git fetch origin $RELEASE_BRANCH:$RELEASE_BRANCH
 developDiff=$(git log HEAD..origin/$DEVELOP_BRANCH --oneline)
 if [[ "$developDiff" != "" ]]; then
     echo "Branch '$DEVELOP_BRANCH' is not up to date."
     exit 1
 fi
-
+releaseDiff=$(git log $RELEASE_BRANCH..origin/$RELEASE_BRANCH --oneline)
+if [[ "$releaseDiff" != "" ]]; then
+    echo "Branch '$RELEASE_BRANCH' is not up to date."
+    exit 1
+fi
 # Create environment variables for release version and snapshot
 export RELEASE="$1"
 export SNAPSHOT="$2"
@@ -27,6 +33,9 @@ git checkout -b "$WORK_PREFIX/$RELEASE" $DEVELOP_BRANCH
 mvn -DnewVersion=$RELEASE -f pom.xml versions:set
 # Commit the pom version changes.
 git commit -am "Updating version for $RELEASE"
+# Finish the release
+git checkout $RELEASE_BRANCH
+git merge --no-edit --no-ff "$WORK_PREFIX/$RELEASE"
 SIGN_TAG=""
 [ $(git config --get user.signingkey) != "" ] && SIGN_TAG="-s"
 git tag $SIGN_TAG -m "Release $RELEASE" "$TAG_PREFIX-$RELEASE"
@@ -37,8 +46,8 @@ git branch -d "$WORK_PREFIX/$RELEASE"
 mvn -DnewVersion=$SNAPSHOT -f pom.xml versions:set
 # Commit the pom version changes
 git commit -am "Updating version for $SNAPSHOT"
-
 # Push master, release, and tags
 git push origin master
+git push origin release
 git push --tags
 echo "All done!"
